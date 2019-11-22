c9f66390703ba5dd1302370b7d83b95e02638a2e
SofianSaleh <56444632+SofianSaleh@users.noreply.github.com>
2019-11-22 20:00:51 +0100

    11

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,28 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var arr = []
+  if(this._storage[index] === undefined){
+  	arr.push(v)
+   this._storage[index] = [v,arr];
+  }else{
+  	
+  }
+  console.log(this._storage)
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+//  for(var i =0; i)
+  return this._storage[index];
+
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  delete this._storage[index]
+
 };
 
 
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,24 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
-setPrototype.add = function(item) {
+setPrototype.add = function(item) {	
+	this._storage[item] = item;
 };
 
 setPrototype.contains = function(item) {
+	for (var key in this._storage) {
+		this._storage[key] === item;
+		return true;
+	}
+	return false;
 };
-
 setPrototype.remove = function(item) {
+	delete this._storage[item];
 };
 
 /*
